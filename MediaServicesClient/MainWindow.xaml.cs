6ef1c292612287c558507df03f4f041242f0203c
Piers <Williams>
2012-09-05 09:19:23 +0100

    First lump of code

---
+++ MediaServicesClient/MainWindow.xaml.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.WindowsAzure.MediaServices;
+using Microsoft.WindowsAzure.MediaServices.Client;
+using System.Diagnostics;
+using System.Collections.ObjectModel;
+using Microsoft.Win32;
+
+namespace MediaServicesClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<String> encodingOptions = new ObservableCollection<String>();
+        OpenFileDialog fileDialog;
+
+        MediaServicesConnector connector;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            connector = new MediaServicesConnector(); 
+
+            fileDialog = new OpenFileDialog();
+            fileDialog.FileOk += fileUpload;
+            connector.HandleContextAcquired += new ContextAcquired(connector_HandleContextAcquired);
+            connector.HandleAssetUploaded += new AssetUploaded(connector_HandleAssetUploaded);
+            connector.HandleAssetDeleted += new AssetDeleted(connector_HandleAssetDeleted);
+            connector.HandleRegularCheck += new RegularCheck(connector_HandleRegularCheck);
+
+            AssetsListBox.ItemsSource = connector.assetsList;
+            foreach (String option in connector.encodingOptions)
+            {
+                encodingOptions.Add(option);
+            }
+            EncodingOptions.ItemsSource = encodingOptions;
+        }
+
+        void connector_HandleRegularCheck()
+        {
+            this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    connector.UpdateAssetList();
+                }
+            ));
+        }
+
+        void connector_HandleAssetDeleted()
+        {
+            this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    connector.UpdateAssetList();
+                }
+            ));
+        }
+
+        void connector_HandleAssetUploaded()
+        {
+            this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    connector.UpdateAssetList();
+                }
+            ));
+        }
+
+        void connector_HandleContextAcquired()
+        {
+            this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    LoggingInPanel.Visibility = System.Windows.Visibility.Collapsed;
+
+                    AccountLabel.Content = AccountNameBox.Text;
+                    AssetsPanel.Visibility = System.Windows.Visibility.Visible;
+                    AccountPanel.Visibility = System.Windows.Visibility.Visible;
+                    MediaServicesPanel.Visibility = System.Windows.Visibility.Visible;
+
+                    connector.UpdateAssetList();
+                }
+            ));
+        }
+
+        private void LoginButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            LoginPanel.Visibility = System.Windows.Visibility.Collapsed;
+            LoggingInPanel.Visibility = System.Windows.Visibility.Visible;
+            connector.AcquireContext(AccountNameBox.Text, AccountKeyBox.Text);          
+        }
+
+        private void ShowDialogButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            fileDialog.ShowDialog();
+        }
+
+        private void UploadAsset_Clicked(object sender, RoutedEventArgs e)
+        {
+            var selections = EncodingOptions.SelectedItems;
+            List<String> options = new List<String>();
+            foreach (var selection in selections)
+            {
+                options.Add(selection as String);
+            }
+            connector.EncodeAsset(options);
+        }
+
+        private void DeleteAsset_Clicked(object sender, RoutedEventArgs e)
+        {
+            List<IAsset> assets = new List<IAsset>();
+            foreach (IAsset asset in AssetsListBox.SelectedItems)
+            {
+                assets.Add(asset);
+            }
+            connector.DeleteAssets(assets);
+        }
+
+        private void fileUpload(System.Object sender, System.EventArgs e)
+        {
+            String fileNames = fileDialog.FileName;
+            connector.UploadAsset(fileNames, AssetCreationOptions.StorageEncrypted);
+
+            EncodingPanel.Visibility = System.Windows.Visibility.Visible;
+
+            MediaServicesPanel.Visibility = System.Windows.Visibility.Collapsed;
+            UploadPanel.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            connector.UpdateAssetList();
+        }
+    }
+}
---
+++ MediaServicesClient/MediaServicesConnector.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Microsoft.WindowsAzure.MediaServices.Client;
+using System.Collections.ObjectModel;
+using System.Data.Services.Client;
+
+namespace MediaServicesClient
+{
+    public delegate void ContextAcquired();
+    public delegate void AssetUploaded();
+    public delegate void AssetDeleted();
+    public delegate void RegularCheck();
+
+
+    public class MediaServicesConnector
+    {
+        private CloudMediaContext context;
+
+        public ObservableCollection<IAsset> assetsList = new ObservableCollection<IAsset>();
+        public List<String> encodingOptions;
+
+        private IAsset currentAsset;
+        private IMediaProcessor mediaProcessor;
+
+        public event ContextAcquired HandleContextAcquired;
+        public event AssetUploaded HandleAssetUploaded;
+        public event AssetDeleted HandleAssetDeleted;
+        public event RegularCheck HandleRegularCheck;
+
+        Boolean checking = true;
+        public MediaServicesConnector()
+        {
+            encodingOptions = getEncodingOptionsAudio();
+        }
+
+        public void AcquireContext(String accountName, String accountKey)
+        {
+            Thread thread = new Thread(() =>
+                {
+                    context = new CloudMediaContext(accountName, accountKey);
+                    //startRegularChecks();
+                    HandleContextAcquired();
+                }
+            );
+            thread.Start();
+        }
+
+        public void UploadAsset(String filePath, AssetCreationOptions assetOption)
+        {
+            if (context == null) throw new ArgumentNullException("context null");
+            if (HandleAssetUploaded == null) throw new Exception("HandleAssetUploaded not subscribed");
+            Thread thread = new Thread(() =>
+                {
+                    currentAsset = context.Assets.Create(filePath, assetOption);
+                    HandleAssetUploaded();
+                }
+            );
+            thread.Start();
+        }
+
+        public void DeleteAsset(IAsset asset)
+        {
+            if (HandleAssetDeleted == null) throw new Exception("HandleAssetDeleted not subscribed");
+            Thread thread = new Thread(() =>
+                {
+                    context.Assets.Delete(asset);
+                    if (HandleAssetDeleted != null)
+                    {
+                        HandleAssetDeleted();
+                    }
+                }
+            );
+            thread.Start();
+        }
+        public void DeleteAssets(List<IAsset> assets)
+        {
+
+            if (HandleAssetDeleted == null) throw new Exception("HandleAssetDeleted not subscribed");
+            Thread thread = new Thread(() =>
+                {
+                    foreach (IAsset asset in assets)
+                    {
+                        context.Assets.Delete(asset);
+                    }
+                    if (HandleAssetDeleted != null)
+                    {
+                        HandleAssetDeleted();
+                    }
+                }
+            );
+
+            thread.Start();
+        }
+        public void DeleteAsset(string assetID)
+        {
+            var asset = GetAssetFromID(assetID);
+            if (asset != null)
+            {
+                DeleteAsset(asset);
+            }
+        }
+
+        private IAsset GetAssetFromID(string assetID)
+        {
+            var asset =
+            from a in context.Assets
+            where a.Id == assetID
+            select a;
+
+            return (asset.First());
+        }
+
+        public void EncodeAsset(IAsset asset, List<String> options)
+        {
+            IJob job = context.Jobs.Create("New Job");
+            mediaProcessor = GetMediaProcessor("Windows Azure Media Encoder");
+
+            foreach (String configOption in options)
+            {
+                ITask task = job.Tasks.AddNew("My encoding task",
+                mediaProcessor,
+                configOption,
+                TaskCreationOptions.None);
+                task.InputMediaAssets.Add(asset);
+                task.OutputMediaAssets.AddNew("Output asset", true, AssetCreationOptions.None);
+            }
+
+            job.Submit();
+        }
+
+        public void EncodeAsset(List<String> options)
+        {
+            EncodeAsset(currentAsset, options);
+        }
+
+        private List<String> getEncodingOptionsAudio()
+        {
+            List<String> options = new List<String>();
+            options.Add("AAC Good Quality Audio");
+            options.Add("AAC High Quality Audio");
+            options.Add("AAC Low Quality Audio");
+            options.Add("WMA Best Quality VBR");
+            options.Add("WMA Good Quality Audio");
+            options.Add("WMA High Quality Audio");
+            options.Add("WMA Lossless 5.1 Audio");
+            options.Add("WMA Low Quality Audio");
+            options.Add("WMA Voice Audio");
+            return options;
+        }
+
+        public void UpdateAssetList()
+        {
+            assetsList.Clear();
+            foreach (IAsset item in context.Assets.ToList())
+            {
+                assetsList.Add(item);
+            }
+        }
+
+        private IMediaProcessor GetMediaProcessor(string mediaProcessor)
+        {
+            var theProcessor =
+                                from p in context.MediaProcessors
+                                where p.Name == mediaProcessor
+                                select p;
+            IMediaProcessor processor = theProcessor.First();
+
+            if (processor == null)
+            {
+                throw new ArgumentException("Unknown Processor");
+            }
+            return processor;
+        }
+
+        private void startRegularChecks()
+        {
+            Thread thread = new Thread(() =>
+                {
+                    while (false)
+                    {
+
+                        Thread.Sleep(1000);
+                        if (context.Assets.ToList().Count() != assetsList.Count)
+                        {
+                            if (HandleRegularCheck != null)
+                            {
+                                HandleRegularCheck();
+                            }
+                        }
+                    }
+                }
+            );
+
+            thread.Start();
+        }
+    }
+}
