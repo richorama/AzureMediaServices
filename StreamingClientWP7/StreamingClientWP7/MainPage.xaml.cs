d54038af5140e4d17941a6418b7d5129c37dae4d
Piers <Williams>
2012-09-07 16:00:41 +0100

    Altered UI slightly

--- StreamingClientWP7/StreamingClientWP7/MainPage.xaml.cs
+++ StreamingClientWP7/StreamingClientWP7/MainPage.xaml.cs
@@ -26,8 +26,8 @@ namespace StreamingClientWP7
             this.Loaded += new RoutedEventHandler(MainPage_Loaded);
 
             PlaylistItem item = new PlaylistItem();
-            //item.MediaSource = new Uri("http://ecn.channel9.msdn.com/o9/content/smf/smoothcontent/bbbwp7/big buck bunny.ism/manifest");
-            item.MediaSource = new Uri("http://media10store.blob.core.windows.net/asset-89533529-dded-42d2-b684-c9025da42b50/dail.ism");
+            item.MediaSource = new Uri("http://ecn.channel9.msdn.com/o9/content/smf/smoothcontent/bbbwp7/big buck bunny.ism/manifest");
+            //item.MediaSource = new Uri("http://media10store.blob.core.windows.net/asset-dbe37166-33f9-4c6a-829b-67a329410c0f/dail.ism/manifest");
             item.DeliveryMethod = Microsoft.SilverlightMediaFramework.Plugins.Primitives.DeliveryMethods.AdaptiveStreaming;
             strmPlayer.Playlist.Add(item);
             strmPlayer.Play(); 
